7ee7381f8d678d43fc3620b33ab1aaf4b67f6d04
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-04 10:07:37 +0700

    update

--- Lecture03/reduce.js
+++ Lecture03/reduce.js
@@ -1,7 +1,10 @@
-const arrayOfNumber = [1, 2, 3, 4];
+const arrayOfNumber = [1, 2, 3, 4, "string"];
 
 const sum = arrayOfNumber.reduce((accumulator, currentValue) => {
     return accumulator + currentValue;
 });
 
 console.log(sum); //10
+
+// get string can run  >>>> 'AAA' >>> "string"
+//
