7a37287bb9152f083c43a0689e49ff3fb232647a
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-04 11:48:47 +0700

    update

---
+++ Lecture03/ex3-2.js
@@ -0,0 +1,13 @@
+let furniture = ['Table', 'Chairs', 'Couch' ] ;
+
+
+let iterator = furniture[Symbol.iterator]();
+
+while (true) {
+    let result = iterator.next();
+    if (result.done) break;
+    console.log(result.value); //outputs character one by one
+}
+
+
+
