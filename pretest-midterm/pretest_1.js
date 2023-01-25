a4ca961490f8d0ed387d402e1311bffa991c0525
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-25 09:39:50 +0700

    update

---
+++ pretest-midterm/pretest_1.js
@@ -0,0 +1,22 @@
+// numberSplit(4) ; //2,2
+// numberSplit(10)  ; //5,5
+// numberSplit(11); //5,6
+// numberSplit(-9) ;  //-5,-4
+
+const numberSplit = (num) => {
+    if (num === undefined) {
+      return "Input Parameter can't be empty"
+    } else {
+    let a = Math.floor(num / 2);
+    let b = num - a;
+    let c = [];
+    c.push(a, b);
+    return c;
+    }
+  };
+
+console.log(numberSplit(4)) ; //2,2
+console.log(numberSplit(10)) ; //5,5
+console.log(numberSplit(11)) ; //5,6
+console.log(numberSplit(-9)) ; //-5,-4
+
---
+++ pretest-midterm/pretest_2.js
@@ -0,0 +1,7 @@
+//higheatDigit(379) //9
+//higheatDigit(2) //2
+//higheatDigit(377401) //7
+
+function higheatDigit(num){
+    
+}
