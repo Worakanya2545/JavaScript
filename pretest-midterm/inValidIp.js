7c11de4d600633b6be15918e53057a110b6dd766
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-25 11:24:06 +0700

    update

---
+++ pretest-midterm/inValidIp.js
@@ -0,0 +1,11 @@
+invalidIP("1", "2", "3", "4"); //true 
+invalidIP("1", "2", "3"); //false
+invalidIP("1", "2", "3", "4","5") ; //fales
+invalidIP("123", "45", "67", "89") ; //true
+invalidIP("123", "456", "78", "90") ; //fales
+invalidIP("123", "045", "067", "089") ; //fales 
+
+
+
+
+
