7d3395723a9ee36d8f05425af80a45a972143e6f
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-25 11:22:11 +0700

    update

---
+++ pretest-midterm/inValdIP.js
@@ -0,0 +1,11 @@
+invaldIP("1", "2", "3", "4"); //true 
+invaldIP("1", "2", "3"); //false
+invaldIP("1", "2", "3", "4","5") ; //fales
+invaldIP("123", "45", "67", "89") ; //true
+invaldIP("123", "456", "78", "90") ; //fales
+invaldIP("123", "045", "067", "089") ; //fales 
+
+
+
+
+
