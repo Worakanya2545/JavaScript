6c494ad2b20410b0cb886fb1d019de4667515ee5
Worakanya2545 <120537400+Worakanya2545@users.noreply.github.com>
2023-01-25 11:11:01 +0700

    update

---
+++ pretest-midterm/removeDupe.js
@@ -0,0 +1,7 @@
+removeDupe([1,0,1,0]) //[1,0]
+removeDupe(["The", "Big", "cat"]) //["the", "big", "cat"]
+removeDupe(["John", "Taylor", "John"]) //["John", "Taylor"]
+//use include
+
+const removeDupe = removeDupe([1,0,1,0]);
+console.log(removeDupe);
